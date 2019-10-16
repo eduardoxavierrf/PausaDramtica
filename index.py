f80b97fd1683defca318e392f45284f7adf2582c
eduardoxavierrf <51022586+eduardoxavierrf@users.noreply.github.com>
2019-10-15 22:57:02 -0300

    Adicionar tela de registro

--- index.py
+++ index.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('teste.html')
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
     return render_template('Login.html')
+
+@app.route('/registrar')
+def registrar():
+    return render_template('registro.html')
 
 
 if __name__ == '__main__':
