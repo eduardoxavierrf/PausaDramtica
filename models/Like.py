7d84d11d1057da03ec6d0ed982478d55ab787bec
*Christian <*cmoreira589@gmail.com*>
2019-10-24 15:54:25 -0300

    Criadas classes representando as funções PesquisarPontos e CriarPontos

--- models/Like.py
+++ models/Like.py
@@ -1,6 +1,44 @@
 import sqlite3
 
 
+class PesquisarPontos:
+    def __init__(self, tabela, id_pontoturistico, name, lugar):
+        self.tabela = tabela
+        self.id_pontoturistico = id_pontoturistico
+        self.name = name
+        self.lugar = lugar
+
+    def pesquisa(self):
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        cursor.execute('''SELECT name, descricao FROM pontos_turisticos WHERE lugar = ?''', (input_pesquisa))
+        rows = cursor.fetchall()
+        connection.close()
+        return rows
+
+
+class CriarPontos:
+    def __init__(self, tabela, id_pontoturistico, name, descricao, lugar):
+        self.tabela = tabela
+        self.id_pontoturistico = id_pontoturistico
+        self.name = name
+        self.descricao = descricao
+        self.lugar = lugar
+
+    def criar(self):
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        cursor.execute('''SELECT name FROM pontos_turisticos WHERE name = ?''', (input_name))
+        rows = cursor.fetchall()
+        connection.close()
+        if(rows == []):
+            connection = sqlite3.connect('database.db')
+            cursor = connection.cursor()
+            cursor.execute('''INSERT INTO pontos_turisticos(name, descricao, lugar) VALUES (?, ?, ?)''', (input_name, input_descricao, input_lugar))
+            connection.commit()
+            connection.close()
+
+
 class LikePontos:
     def __init__(self, tabela, id_pontoturistico, id_usuario, data, avaliacao):
         self.tabela = tabela
@@ -31,3 +69,4 @@ class LikePontos:
             cursor.execute('''SELECT * FROM {} WHERE {}=?'''.format(self.tabela, item), (kwargs[item]))
             rows = cursor.fetchall()
             return rows
+
